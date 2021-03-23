ebd3d1c27798d730b2dd4d760b0d05e59fb73af7
Omar Tarek <omar.tarek@areebgroup.com>
2021-03-23 23:25:16 +0200

    Applaied Navigation to Details Screen Added Layout Enum

--- The Hitchhiker Prophecy/Modules/Home/Shared Views/Characters List View/CharactersListView.swift
+++ The Hitchhiker Prophecy/Modules/Home/Shared Views/Characters List View/CharactersListView.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
+protocol CharactersListViewDelegate: AnyObject {
+    func charactersListView(charactersListView: CharactersListView, didTapOnCharacterWith index: Int)
+}
+
 class CharactersListView: UIView, NibLoadable {
+    
+    // MARK: - Nested Type
+    
+    enum Layout {
+        case grid(frame: CGRect)
+        case list(frame: CGRect)
+    }
     
     // MARK: - IBOutlets
     
@@ -16,38 +27,18 @@ class CharactersListView: UIView, NibLoadable {
         didSet{
             charactersCollectionView.delegate = self
             charactersCollectionView.dataSource = self
-            charactersCollectionView.collectionViewLayout = listLayout
+            charactersCollectionView.collectionViewLayout = currentLayout.collectionViewFlowLayout
             charactersCollectionView.registerCell(withCellType: CharacterCollectionViewCell.self)
         }
     }
     
     // MARK: - Properties
     
+    weak var delegate: CharactersListViewDelegate?
+    
     private var characters: [HomeScene.Search.ViewModel] = []
-    
-    private lazy var listLayout: UICollectionViewFlowLayout = {
-
-        let collectionFlowLayout = UICollectionViewFlowLayout()
-        collectionFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
-        collectionFlowLayout.itemSize = CGSize(width: frame.width, height: 300)
-        collectionFlowLayout.minimumInteritemSpacing = 0
-        collectionFlowLayout.minimumLineSpacing = 0
-        collectionFlowLayout.scrollDirection = .vertical
-        return collectionFlowLayout
-    }()
-
-    private lazy var gridLayout: UICollectionViewFlowLayout = {
         
-        let collectionFlowLayout = UICollectionViewFlowLayout()
-        collectionFlowLayout.scrollDirection = .vertical
-        collectionFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
-        collectionFlowLayout.itemSize = CGSize(width: (frame.width - 80) / 2 , height: frame.height*0.16)
-        collectionFlowLayout.minimumInteritemSpacing = 20
-        collectionFlowLayout.minimumLineSpacing = 20
-        return collectionFlowLayout
-    }()
-    
-    private var isListView = true
+    private lazy var currentLayout = Layout.list(frame: frame)
     
     // MARK: - Life Cycle Functions
     
@@ -65,9 +56,8 @@ class CharactersListView: UIView, NibLoadable {
     }
     
     func switchLayout() {
-        let currentLayout = isListView ? gridLayout : listLayout
-        isListView.toggle()
-        charactersCollectionView.startInteractiveTransition(to: currentLayout, completion: nil)
+        currentLayout.toggle()
+        charactersCollectionView.startInteractiveTransition(to: currentLayout.collectionViewFlowLayout, completion: nil)
         charactersCollectionView.finishInteractiveTransition()
     }
     
@@ -84,4 +74,41 @@ extension CharactersListView: UICollectionViewDelegate, UICollectionViewDataSour
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        delegate?.charactersListView(charactersListView: self, didTapOnCharacterWith: indexPath.row)
+    }
+}
+
+extension CharactersListView.Layout {
+    
+    var collectionViewFlowLayout: UICollectionViewFlowLayout {
+        switch self {
+        case .grid(let frame):
+            let collectionFlowLayout = UICollectionViewFlowLayout()
+            collectionFlowLayout.scrollDirection = .vertical
+            collectionFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
+            collectionFlowLayout.itemSize = CGSize(width: (frame.width - 80) / 2 , height: frame.height*0.16)
+            collectionFlowLayout.minimumInteritemSpacing = 20
+            collectionFlowLayout.minimumLineSpacing = 20
+            return collectionFlowLayout
+            
+        case .list(let frame):
+            let collectionFlowLayout = UICollectionViewFlowLayout()
+            collectionFlowLayout.sectionInset = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
+            collectionFlowLayout.itemSize = CGSize(width: frame.width, height: 300)
+            collectionFlowLayout.minimumInteritemSpacing = 0
+            collectionFlowLayout.minimumLineSpacing = 0
+            collectionFlowLayout.scrollDirection = .vertical
+            return collectionFlowLayout
+        }
+    }
+    
+    mutating func toggle() {
+        switch self {
+        case .grid(let frame):
+            self = .list(frame: frame)
+        case .list(let frame):
+            self = .grid(frame: frame)
+        }
+    }
 }
