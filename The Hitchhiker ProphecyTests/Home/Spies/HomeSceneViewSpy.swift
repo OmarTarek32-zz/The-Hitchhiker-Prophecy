a3b62cb9ecf55631e3f77bd4915a4795b2940f3f
Omar Tarek <omar.tarek@areebgroup.com>
2021-03-24 11:19:18 +0200

    Added spy view and Test Data file

---
+++ The Hitchhiker ProphecyTests/Home/Spies/HomeSceneViewSpy.swift
@@ -0,0 +1,38 @@
+//
+//  HomeSceneViewSpy.swift
+//  The Hitchhiker ProphecyTests
+//
+//  Created by Omar Tarek on 3/24/21.
+//  Copyright © 2021 SWVL. All rights reserved.
+//
+
+import Foundation
+@testable import The_Hitchhiker_Prophecy
+
+class HomeSceneViewSpy: HomeSceneDisplayView {
+    
+    var interactor: HomeSceneBusinessLogic?
+    var router: HomeSceneRoutingLogic?
+    
+    var charactersViewModel: [HomeScene.Search.ViewModel]?
+    var error: Error?
+    var isShowingLoadingIndicator = false
+    
+    func didFetchCharacters(viewModel: [HomeScene.Search.ViewModel]) {
+        charactersViewModel = viewModel
+    }
+    
+    func failedToFetchCharacters(error: Error) {
+        self.error = error
+    }
+    
+    func showLoading() {
+        isShowingLoadingIndicator = true
+    }
+    
+    func hideLoading() {
+        isShowingLoadingIndicator = false
+    }
+    
+    
+}
---
+++ The Hitchhiker ProphecyTests/Home/Test Data/CharactersTestData.swift
@@ -0,0 +1,9 @@
+//
+//  CharactersTestData.swift
+//  The Hitchhiker ProphecyTests
+//
+//  Created by Omar Tarek on 3/24/21.
+//  Copyright © 2021 SWVL. All rights reserved.
+//
+
+import Foundation
